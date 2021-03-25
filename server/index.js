59877b418ee715d620e848dc392263e6fa5e46ea
akshay <kokaa2@hawkmail.newpaltz.edu>
2021-03-25 14:20:22 -0400

    error handling

--- server/index.js
+++ server/index.js
@@ -1,16 +1,33 @@
 /*  B"H
 */
 
+const path = require('path');
 const express = require('express');
+
+const usersCtrl = require('./controllers/users');
+const postsCtrl = require('./controllers/posts');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app
-    .get('/', (req, res) => {
-    res.send('Hello!')
+    .use(express.json())
+    .use(express.static('./docs'))
+
+    .use('/users', usersCtrl)
+    .use('/posts', postsCtrl)
+
+
+    // All the way at the end of the pipeline. Return instead of not found
+    .get('*', (req, res) => {
+        res.sendFile( path.join(__dirname, '../docs/index.html' ) );
     })
-    .get('/purim', (req, res) => {
-        res.send('Astala vista!');
+
+    .use((error, req, res, next)=>{
+      console.error(error);
+
+        res.status(error.code || 500 );
+        res.send( { msg: error.msg });
     })
 
 app.listen(port, () => {
